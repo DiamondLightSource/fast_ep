6ff9e8e9e1f70b791cef930178ad678adfb36fc0
Jun Aishima <jun.aishima@diamond.ac.uk>
2013-06-14 14:08:25 +0000

    MXWS-42 implement xml output
     * create phasing statistics XML creation method
     * implement formatting of main ispyb.xml
     * also store command-line and write directory
     * http://jira.diamond.ac.uk/browse/MXSW-42

--- lib/xml_output.py
+++ lib/xml_output.py
@@ -1,12 +1,12 @@
 import os
 
-def write_ispyb_xml(filename, xml_results):
+def write_ispyb_xml(filename, full_command_line, write_directory, xml_results):
     '''Write items in the _xml_results into an XML file to be stored in ISPyB'''
-    xml_template = os.path.join(os.environ['FAST_DP_ROOT'],
+    xml_template = os.path.join(os.environ['FAST_EP_ROOT'],
                                 'lib', 'templates', 'ispyb.xml')
-    phasing_statistics_fom_template = os.path.join(os.environ['FAST_DP_ROOT'],
+    phasing_statistics_fom_template = os.path.join(os.environ['FAST_EP_ROOT'],
                                 'lib', 'templates', 'phasing_statistics_fom.xml')
-    phasing_statistics_mapcc_template = os.path.join(os.environ['FAST_DP_ROOT'],
+    phasing_statistics_mapcc_template = os.path.join(os.environ['FAST_EP_ROOT'],
                                 'lib', 'templates', 'phasing_statistics_mapcc.xml')
 
     if not os.path.exists(xml_template):
@@ -19,7 +19,62 @@ def write_ispyb_xml(filename, xml_results):
         print 'XML template not found: %s' % phasing_statistics_mapcc_template
         return
 
+    #get phasing statistics from xml_results
+    all_phasing_statistics_fom = ""
+    all_phasing_statics_mapcc = ""
+    (all_phasing_statistics_fom, all_phasing_statistics_mapcc) = get_phasing_statistics(phasing_statistics_fom_template, phasing_statistics_mapcc_template, xml_results)
+    
     open(filename, 'w').write(
         open(xml_template, 'r').read().format(
-            spacegroup = xml_results['SPACEGROUP']
+            commandline = full_command_line,
+            results_directory = write_directory,
+            spacegroup_id = xml_results['SPACEGROUP'],
+            solvent_content = xml_results['SOLVENTCONTENT'],
+            enantiomorph = xml_results['ENANTIOMORPH'],
+            lowres = xml_results['LOWRES'],
+            highres = xml_results['HIGHRES'],
+            shelxc_spacegroup = xml_results['SHELXC_SPACEGROUP_ID'],
+            phasing_statistics_fom = all_phasing_statistics_fom,
+            phasing_statistics_mapcc = all_phasing_statistics_mapcc
             ))
+
+def get_phasing_statistics(fom_template, cc_template, xml_results):
+    total_bins = 1
+    all_phasing_statistics_fom = ""
+    all_phasing_statistics_mapcc = ""
+
+    #find number of bins - use RESOLUTION_LOW as the field to check for this
+    done = False
+    while not done:
+        bin_number_name = str(total_bins).zfill(2)
+        try:
+            resolution_low = xml_results['RESOLUTION_LOW'+bin_number_name]
+        except KeyError:
+            done = True
+            continue
+        total_bins += 1
+    for bin_number in xrange(total_bins):
+        bin_number_name = str(bin_number).zfill(2)
+        resolution_low = float(xml_results['RESOLUTION_LOW'+bin_number_name])
+        resolution_high = float(xml_results['RESOLUTION_HIGH'+bin_number_name])
+        fom = float(xml_results['FOM' + bin_number_name])
+        mapcc = float(xml_results['MAPCC' + bin_number_name])
+        nreflections = int(xml_results['NREFLECTIONS' + bin_number_name])
+        if resolution_low == None or resolution_high == None or fom == None or mapcc == None or nreflections == None:
+            raise RuntimeError, "One of the fields is empty, this should not be happening."
+        all_phasing_statistics_fom += open(fom_template,'r').read().format(
+                bin_number = bin_number + 1, #start bin numbers at 1 for the XML file
+                number_bins = total_bins,
+                bin_low_res = resolution_low,
+                bin_high_res = resolution_high,
+                bin_fom = fom,
+                num_refl = nreflections)
+        all_phasing_statistics_mapcc +=  open(cc_template,'r').read().format(
+                bin_number = bin_number + 1,
+                number_bins = total_bins,
+                bin_low_res = resolution_low,
+                bin_high_res = resolution_high,
+                bin_map_cc = mapcc,
+                num_refl = nreflections)
+    return (all_phasing_statistics_fom, all_phasing_statistics_mapcc)
+        
